e2c35ff36102cabfa24c7fd1f53d1581ed02cf06
abeuken <vandb.amy@gmail.com>
2024-02-22 19:03:47 +0800

    .

---
+++ Project1ver1/README.md
@@ -0,0 +1,37 @@
+Higher or Lower Card Game
+
+Rules of the game:
+
+From the first random card generated, guess whether the next card will be higher or lower.
+If the card is of the same value, it is a DRAW.
+Player earns points for guessing correctly, loses when wrong.
+The card deck removes cards as you continue guessing!
+
+Technologies used:
+HTML, CSS, Javascript
+
+Let's Play!
+
+# Screenshot
+
+<img src="url to your image on imgur">
+<img src="url to your image on imgur">
+
+# Technologies Used
+
+- JavaScript
+- HTML
+- CSS
+- CardStarter CSS Library
+
+# Getting Started
+
+[Click to Play Connect Four!](your deployment url here)
+
+# Next Steps
+
+-Using images of a deck of cards
+-Timed game
+
+References:
+https://www.rapidtables.com/web/html/html-codes/html-code-heart.html
--- Project1ver1/script.js
+++ Project1ver1/script.js
@@ -1,33 +1,34 @@
+//creating variables here that can relate JS+HTMl and also create functions here using variable names
 const message = document.querySelector(".message");
 const score = document.querySelector(".score");
 const button = document.querySelectorAll("button");
 const gameplay = document.querySelector(".gameplay");
 
-let curCardValue = 0; //this contains whatever the existing card is
+let curCardValue = 0; //this contains whatever the existing card is, can be seen by player
 let scoreValue = 0;
-let deck = []; //global variable which will produce a deck of cards in JS
+let deck = []; //default variables to be used to produce deck by JS
 const ranks = [2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K", "A"];
-const suits = ["hearts", "diams", "clubs", "spades"];
-
+const suits = ["hearts", "diams", "clubs", "spades"]; //these data is needed to produce the deck
 for (let i = 0; i < button.length; i++) {
   console.log(button[i]);
-  button[i].addEventListener("click", playGame);
+  button[i].addEventListener("click", playGame); //add eventlisteners to buttons to make buttons functional
 }
-
+//default fn for buttons, one view is when start clicked, it hides and shows higher/lower and again
 function toggleButtons() {
-  button[0].classList.toggle("hideButton");
+  button[0].classList.toggle("hideButton"); //refer to hidebutton display on css
   button[1].classList.toggle("hideButton");
   button[2].classList.toggle("hideButton");
 }
-
+//create a fn triggered by an event(button click), this is where gameplay starts
 function playGame(e) {
   let temp = e.target.innerText;
-  let myCard = drawCard();
-
+  let myCard = drawCard(); //calls drawCard fn to get random card from deck(seen by player),myCard is waiting to be drawn
   if (temp == "Start") {
     message.innerHTML = "Higher or Lower";
-    gameplay.innerHTML = "";
-    makeCard(myCard);
+    gameplay.innerHTML = ""; //used to clear game area
+    scoreValue = 0; //this and line below resets the score to 0 only when 'start' is clicked
+    score.innerHTML = scoreValue;
+    makeCard(myCard); //this generates the visuals of the card
     toggleButtons();
     return;
   }
@@ -49,16 +50,18 @@ function playGame(e) {
   }
   makeCard(myCard);
 }
+//this fn randomly selects card from deck and removes it
 function drawCard() {
   if (deck.length > 0) {
-    let randIndex = Math.floor(Math.random() * deck.length);
-    let card = deck.splice(randIndex, 1)[0]; //item removed from deck, until zero, continously shrinking
+    let randIndex = Math.floor(Math.random() * deck.length); //math function reeturns a random integer from 0 to deck length:
+    let card = deck.splice(randIndex, 1)[0]; //item removed from deck, until zero, splice() to remove elements without leaving "holes" in the array
     return card;
   } else {
-    makeDeck(); //will come down to this and create new deck, infinity
+    makeDeck(); //will come down to this and  when deck empties it calls makeDeck to create
     return drawCard();
   }
 }
+//this function initialise the deck, iterates over each suit and length, card has suit and rank
 function makeDeck() {
   deck = [];
   for (let i = 0; i < suits.length; i++) {
@@ -72,29 +75,32 @@ function makeDeck() {
   }
   console.log(deck);
 }
-//this is the visuals of the card
+//this codeblock below are for the visuals of the card
 function makeCard(card) {
   console.log(card);
-  let html1 = card.rank + "<br>&" + card.suit + ";";
+
   let html2 = card.rank + "&" + card.suit + ";";
   let curCards = document.querySelectorAll(".card");
 
-  let div = document.createElement("div");
+  let div = document.createElement("div"); //div created here
   div.setAttribute("class", "card");
   div.style.left = curCards.length * 25 + "px";
   curCardValue = card.value;
   if (card.suit === "hearts" || card.suit === "diams") {
     div.classList.add("red");
   }
-
+  //93-95 is span1
   let span1 = document.createElement("span");
   span1.setAttribute("class", "tiny");
   span1.innerHTML = html2;
-  div.appendChild(span1);
+  div.appendChild(span1); //span 1 in to div line 85
 
+  //create span2 and append to line 85
   let span2 = document.createElement("span");
   span2.setAttribute("class", "big");
+  let html1 = card.rank + "<br>&" + card.suit + ";";
   span2.innerHTML = html1;
+  span2.innerHTML = card.rank + "<br>&" + card.suit + ";";
   div.appendChild(span2);
 
   gameplay.appendChild(div);
